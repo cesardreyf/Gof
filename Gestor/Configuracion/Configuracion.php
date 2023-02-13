a8549168371773d605a0ba54b4f30695062b50e6
Dreyf <dreyfcesar@gmail.com>
2023-02-13 10:58:14 -0300

    Documenta el gestor de configuración
    Agrega un poco de documentación a las funciones para que mi yo del
    futuro no se me enoje...

--- Gestor/Configuracion/Configuracion.php
+++ Gestor/Configuracion/Configuracion.php
@@ -11,11 +11,23 @@ class Configuracion implements IConfiguracion
      */
     private $marcas;
 
+    /**
+     *  Crea una instancia de Configuracion
+     *
+     *  @param int $estadoInicial Valor que tendrá la máscara de bits interna
+     */
     public function __construct(int $estadoInicial = 0)
     {
         $this->marcas = $estadoInicial;
     }
 
+    /**
+     *  Activa los bits pasados por el argumento
+     *
+     *  @param int $bits Bits a activarse en la máscara de bits interna
+     *
+     *  @return int Devuelve el estado actual de la máscara de bits interna
+     */
     public function activar(int $bits, int ...$otros): int
     {
         foreach( $otros as $masBits ) {
@@ -25,6 +37,13 @@ class Configuracion implements IConfiguracion
         return $this->marcas |= $bits;
     }
 
+    /**
+     *  Desactiva los bits pasados por el argumento
+     *
+     *  @param int $bits Bits a desactivarse en la máscara de bits interna
+     *
+     *  @return int Devuelve el estado actual de la máscara de bits interna
+     */
     public function desactivar(int $bits, int ...$otros): int
     {
         foreach( $otros as $masBits ) {
@@ -34,6 +53,13 @@ class Configuracion implements IConfiguracion
         return $this->marcas &= ~$bits;
     }
 
+    /**
+     *  Valida si los bits pasados por argumento están activos
+     *
+     *  @param int $bits Bit o bits a validar si están activos
+     *
+     *  @return int Devuelve el estado actual de la máscara de bits interna
+     */
     public function activados(int $bits, int ...$otros): bool
     {
         foreach( $otros as $masBits ) {
@@ -43,6 +69,13 @@ class Configuracion implements IConfiguracion
         return ($this->marcas & $bits) === $bits;
     }
 
+    /**
+     *  Valida si los bits pasados por argumento están desactivados
+     *
+     *  @param int $bits Bit o bits a validar si están desactivados
+     *
+     *  @return int Devuelve el estado actual de la máscara de bits interna
+     */
     public function desactivados(int $bits, int ...$otros): bool
     {
         foreach( $otros as $masBits ) {
@@ -52,11 +85,23 @@ class Configuracion implements IConfiguracion
         return ($this->marcas & $bits) === 0;
     }
 
+    /**
+     *  Define el valor de la máscara de bits
+     *
+     *  @param int $valor Nuevo valor que tendrá la máscara de bits
+     *
+     *  @return int Devuelve el estado actual de la máscara de bits interna
+     */
     public function definir(int $valor): int
     {
         return $this->marcas = $valor;
     }
 
+    /**
+     *  Obitene el valor de la máscara de bits
+     *
+     *  @return int Devuelve el estado actual de la máscara de bits interna
+     */
     public function obtener(): int
     {
         return $this->marcas;
