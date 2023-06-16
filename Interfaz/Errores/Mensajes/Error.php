1e053dd85c847f72ef24a936fa63a9a3b1d595fe
Dreyf <dreyfcesar@gmail.com>
2023-06-16 09:37:03 -0300

    Agrega el método 'limpiar' a la interfaz de Errores\Mensaje
    Agrega el método **limpiar** a la interfaz y crea la documentación
    DOCBLOCK inexistente.

--- Interfaz/Errores/Mensajes/Error.php
+++ Interfaz/Errores/Mensajes/Error.php
@@ -4,9 +4,40 @@ namespace Gof\Interfaz\Errores\Mensajes;
 
 use Gof\Interfaz\Lista;
 
+/**
+ * Interfaz para mensajes de errores
+ *
+ * @package Gof\Interfaz\Errores\Mensajes
+ */
 interface Error extends Lista
 {
+    /**
+     * Limpia los errores
+     */
+    public function limpiar();
+
+    /**
+     * Indica si hay errores
+     *
+     * @return bool Devuelve **true** si existen errores o **false** de lo contrario.
+     */
     public function hay(): bool;
+
+    /**
+     * Obtiene o define el código de error
+     *
+     * @param ?int $error Código de error o **null** para obtener el actual.
+     *
+     * @return int Devuelve el código de error actual.
+     */
     public function codigo(?int $error = null): int;
+
+    /**
+     * Obtiene o define el mensaje de error
+     *
+     * @param ?string $error Mensaje de error o **null** para obtener el actual.
+     *
+     * @return string Devuelve el mensaje de error actual.
+     */
     public function mensaje(?string $error = null): string;
 }
