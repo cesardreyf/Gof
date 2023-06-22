ee29ab15b969d7ad373fd5b0b353fadb31019f80
Dreyf <dreyfcesar@gmail.com>
2023-06-21 22:29:10 -0300

    Crea las pruebas unitarias para el mediador de errores
    Crea la prueba unitaria para el encargado de guardar los códigos y
    mensajes de errores en los campos.

---
+++ test/Sistema/Formulario/Mediador/Campo/ErrorTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Sistema\Formulario\Mediador\Campo;
+
+use Gof\Datos\Errores\Mensajes\Error as ErrorDeCampo;
+use Gof\Sistema\Formulario\Interfaz\Campo;
+use Gof\Sistema\Formulario\Mediador\Campo\Error;
+use PHPUnit\Framework\TestCase;
+
+class ErrorTest extends TestCase
+{
+
+    public function testEscribirCodigoYMensajeDeErrorEnElCampoAlReportar(): void
+    {
+        $campo = $this->createMock(Campo::class);
+        $error = $this->createMock(ErrorDeCampo::class);
+
+        $codigoDeError = PHP_INT_MAX;
+        $mensajeDeError = 'algo';
+
+        $error
+            ->expects($this->once())
+            ->method('codigo')
+            ->with($codigoDeError);
+
+        $error
+            ->expects($this->once())
+            ->method('mensaje')
+            ->with($mensajeDeError);
+
+        $campo
+            ->method('error')
+            ->willReturn($error);
+
+        Error::reportar($campo, $mensajeDeError, $codigoDeError);
+    }
+
+}
