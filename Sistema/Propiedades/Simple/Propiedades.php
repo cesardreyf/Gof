04747b737ed73186957836f1ff3c5120c984c41c
Dreyf <dreyfcesar@gmail.com>
2023-06-23 08:51:43 -0300

    Cambia la documentación del sistem propiedades
    Lo acabo de leer después de cierto tiempo y no entendí nada.
    
    Se eliminó unas líneas innecesarias y quedó un poco mejor la documentación.

--- Sistema/Propiedades/Simple/Propiedades.php
+++ Sistema/Propiedades/Simple/Propiedades.php
@@ -10,19 +10,11 @@ use Gof\Sistema\Propiedades\Simple\Modulos\Persistencia;
 /**
  * Sistema de gestion de propiedades
  *
- * Sistema que gestiona las operaciones básicas de datos (Persistencia, Actualizacion, Borrado
- * y Obtención).
+ * Sistema que gestiona las operaciones básicas de datos (Persistencia,
+ * Actualizacion, Borrado y Obtención).
  *
- * Cada operación está dividida en un módulo diferente donde se almacenan las propiedades que
- * cumplirán con dicha función. Cada módulo almacena una lista de errores independiente y otra
- * de propiedades.
- *
- * Este sistema es simple y solo sirve para abstraer las operaciones sobre elementos de un
- * conjunto de datos. La implementación de lo que deben hacer no corresponde al sistema sino
- * a los objetos de la lista de propiedades de cada módulo.
- * Por ejemplo, al agregar una propiedad al módulo de persistencia este se encargará de
- * persistir los datos cuando el sistema llame al método persistir de dicho elemento.
- * El sistema solo gestionará el momento y los errores.
+ * Cada operación está dividida en un módulo diferente donde se almacenan las
+ * propiedades y una lista de errores.
  *
  * @package Gof\Sistema\Propiedades\Simple
  */
