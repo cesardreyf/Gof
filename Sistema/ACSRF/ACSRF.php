ca58b3fd6236038b38c2eee41a589bdc99f67e1e
Dreyf <dreyfcesar@gmail.com>
2023-07-03 16:16:31 -0300

    Agrega soporte para cookies en el sistema anti csrf
    Implementa el soporte parar enviar y validar hashes mediante las cookies.

--- Sistema/ACSRF/ACSRF.php
+++ Sistema/ACSRF/ACSRF.php
@@ -2,6 +2,7 @@
 
 namespace Gof\Sistema\ACSRF;
 
+use Gof\Contrato\Cookies\Cookies;
 use Gof\Contrato\Session\Session;
 
 /**
@@ -19,6 +20,13 @@ class ACSRF
     private Session $sesion;
 
     /**
+     * Instancia del gestor de cookies
+     *
+     * @var Cookies
+     */
+    private Cookies $cookies;
+
+    /**
      * Clave que usará el token
      *
      * @var string
@@ -26,13 +34,43 @@ class ACSRF
     public string $clave = 'acsrf_token';
 
     /**
+     * Clave que se usará para obtener o definir el hash en el cliente
+     *
+     * El hash se enviará o obtendrá mediante las cookies empleando esta clave
+     * asociada.
+     *
+     * @var string
+     */
+    public string $claveCookie = 'acsrf_token';
+
+    /**
+     * Almacena temporalmente el último hash generado
+     *
+     * @var ?string
+     */
+    private ?string $hashTemporal;
+
+    /**
+     * Duración que tendrá la cookie enviada
+     *
+     * Cuando se envíe un hash por cookies la misma durará lo que se indique
+     * aquí.  Si esta variable es igual a 0 la duración será la que tenga por
+     * defecto el gestor de cookies.
+     *
+     * @var int
+     */
+    public int $duracion = 0;
+
+    /**
      * Constructor
      *
      * @param Session $sesion Gestor de sesión
+     * @param Cookies $cookies Gestor de cookies
      */
-    public function __construct(Session $sesion)
+    public function __construct(Session $sesion, Cookies $cookies)
     {
         $this->sesion = $sesion;
+        $this->cookies = $cookies;
     }
 
     /**
@@ -45,8 +83,8 @@ class ACSRF
      */
     public function generar(): string
     {
-        $hash = md5(uniqid(mt_rand(), true));
-        return $this->sesion->definir($this->clave, $hash);
+        $hashTemporal = md5(uniqid(mt_rand(), true));
+        return $this->sesion->definir($this->clave, $hashTemporal);
     }
 
     /**
@@ -67,12 +105,43 @@ class ACSRF
         return $hashInterno === $hash;
     }
 
-    /*
-     * Envía el hash por cookie
+    /**
+     * Valida el hash obteniéndolo desde una cookie
+     *
+     * Obtiene el hash automáticamente desde una cookie.
+     *
+     * @return bool Devuelve **true** si el hash es válido o **false** de lo contrario.
      */
-    // public function enviarCookie()
-    // {
-    //     $this->cookie->definir($this->claveCookie, $this->generar());
-    // }
+    public function validarDesdeCookies(): bool
+    {
+        $hash = $this->cookies->obtener($this->claveCookie);
+
+        if( is_null($hash) ) {
+            return false;
+        }
+
+        return $this->validar($hash);
+    }
+
+    /**
+     * Envía el hash por cookies
+     *
+     * Envía una cookie al cliente con el hash generado.
+     */
+    public function enviarPorCookies()
+    {
+        $expire = 0;
+        if( $this->duracion !== 0 ) {
+            $expire = $this->cookies->duracion();
+            $this->cookies->duracion($this->duracion, true);
+        }
+
+        $hash = $this->hashTemporal ?? $this->generar();
+        $this->cookies->definir($this->claveCookie, $hash);
+
+        if( $this->duracion !== 0 ) {
+            $this->cookies->duracion($expire, false);
+        }
+    }
 
 }
