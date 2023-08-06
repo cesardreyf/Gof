51c5a6d8b8fb7116a871cb238a100e5a9fac87f2
Dreyf <dreyfcesar@gmail.com>
2023-08-06 10:28:23 -0300

    (Sistema MVC) Brinda soporte para métodos dinámicos en el DAP N3
    Cambia la clase padre del que extiende el DAP de nivel 3 para dar
    soporte a métodos dinámicos.

--- Sistema/MVC/Aplicacion/DAP/N3.php
+++ Sistema/MVC/Aplicacion/DAP/N3.php
@@ -2,13 +2,13 @@
 
 namespace Gof\Sistema\MVC\Aplicacion\DAP;
 
-use Gof\Gestor\Dependencias\Simple\Dependencias;
+use Gof\Gestor\Dependencias\Simple\DependenciasMagicas;
 
 /**
  * Datos de Acceso Público de nivel 3
  *
  * @package Gof\Sistema\MVC\Aplicacion\DAP
  */
-class N3 extends Dependencias implements DAP
+class N3 extends DependenciasMagicas implements DAP
 {
 }
